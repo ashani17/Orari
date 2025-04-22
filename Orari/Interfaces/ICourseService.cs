d252c0c87dd18b62f814a5dc4c246ccf128fc460
Albi <Albi@DESKTOP-1176KEP>
2025-04-22 22:03:38 +0200

    Added CourseService

---
+++ Orari/Interfaces/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using Orari.Models;
+
+namespace Orari.Interfaces
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<Courses>> GetAllCourses();
+        Task<Courses> GetCourseByIdAsync(int id);
+        Task<Courses?> GetCourseByNameAsync(string CName);
+        Task<Courses> CreateCourseAsync(Courses course, string CName);
+        Task<bool> UpdateCourseAsync(int id, Courses course);
+        Task<bool> DeleteCourseAsync(int id);
+    }
+}
---
+++ Orari/Services/CourseServices.cs
@@ -0,0 +1,59 @@
+﻿using Orari.Interfaces;
+using Orari.Models;
+
+namespace Orari.Services
+{
+    public class CourseServices : ICourseService
+    {
+        private readonly ICourseService _course;
+        public CourseServices(ICourseService course)
+        {
+            _course = course;
+        }
+        public async Task<Courses> CreateCourseAsync(Courses course, string CName)
+        {
+            var existingCourse = GetCourseByNameAsync(CName);
+            if (existingCourse != null)
+            {
+                throw new Exception("Course already exists");
+            }
+
+            return await _course.CreateCourseAsync(course, CName);
+        }
+
+        public Task<bool> DeleteCourseAsync(int id)
+        {
+            var existingCourse = GetCourseByIdAsync(id);
+            if (existingCourse == null)
+            {
+                throw new Exception("Course not found");
+            }
+            return _course.DeleteCourseAsync(id);
+        }
+
+        public Task<IEnumerable<Courses>> GetAllCourses()
+        {
+            return _course.GetAllCourses();
+        }
+
+        public Task<Courses> GetCourseByIdAsync(int id)
+        {
+            return _course.GetCourseByIdAsync(id);
+        }
+
+        public Task<Courses?> GetCourseByNameAsync(string CName)
+        {
+            return _course.GetCourseByNameAsync(CName);
+        }
+
+        public Task<bool> UpdateCourseAsync(int id, Courses course)
+        {
+            var existingCourse = GetCourseByIdAsync(id);
+            if (existingCourse == null)
+            {
+                throw new Exception("Course not found");
+            }
+            return _course.UpdateCourseAsync(id, course);
+        }
+    }
+}
